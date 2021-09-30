ebf6a20de5e5c6748019737251caf2fc65f7a92b
Theo <theologos.baxevanos@students.ffhs.ch>
2021-09-30 22:53:55 +0200

    refactoring frontend

---
+++ app/frontend/src/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Dashboard() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <h1>Game here</h1>
+            <h1>Chat here</h1>
+            <a href="/users/logout">Logout</a>
+        </div>
+    )
+}
+
+export default Dashboard
---
+++ app/frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <h1>Coronattack Home</h1>
+            <a href="/users/login">Login</a>
+        </div>
+    )
+}
+
+export default Home;
