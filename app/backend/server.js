803f0159e5f87d706600a32f04b2c81e8ac3c057
Theo <theologos.baxevanos@students.ffhs.ch>
2021-10-01 00:20:01 +0200

    refactoring backend

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
     res.send("Hello from backend!");
-});
+});  */
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
 
 app.listen(PORT, () => {
     console.log(`backend runs on port ${PORT}`);
--- app/frontend/src/pages/Home.js
+++ app/frontend/src/pages/Home.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import { React, useState, useEffect } from "react";
 
 function Home() {
+
+    const [data, setData] = useState(null);
+    useEffect(() => {
+        fetch("/api")
+            .then((res) => res.json())
+            .then((data) => setData(data.message));
+    }, []);
+
     return (
         <div>
             <h1>Coronattack Home</h1>
             <a href="/users/login">Login</a>
+            <p>{!data ? "Loading..." : data}</p>
         </div>
     )
 }
