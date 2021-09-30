1cec07e3905079cb5c5660d82ada91c93d6509a5
Theo <theologos.baxevanos@students.ffhs.ch>
2021-09-30 22:40:08 +0200

    refactoring

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,16 +1,12 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-const staticFileMiddleware = express.static(path.join(__dirname + '/build'));
 
-app.use(staticFileMiddleware);
+const PORT = process.env.PORT || 8080;
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '/build', 'index.html'));
+app.get('/', (req, res) => {
+    res.send("Hello from backend!");
 });
 
-let server = app.listen(process.env.PORT || 3000, function () {
-    let port = server.address().port;
-    console.log("App now running on port", port);
+app.listen(PORT, () => {
+    console.log(`backend runs on port ${PORT}`);
 });
