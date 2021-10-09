847909957d2823fd632605daa6c5655a1cf3fbc5
chantifa <chantifa@windowslive.com>
2021-10-09 11:25:22 +0200

    docker update

--- app/backend/storage/ddl.sql
+++ app/backend/storage/ddl.sql
@@ -7,6 +7,6 @@ CREATE TABLE IF NOT EXISTS users
     name VARCHAR(200) NOT NULL,
     email VARCHAR(200) NOT NULL, 
     password VARCHAR(200) NOT NULL, 
-    UNIQUE (email); 
+    UNIQUE (email)
 );
 
