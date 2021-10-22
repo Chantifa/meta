d8c08f78497ffb5d8c32a6eaf46396c37500bf9f
chantifa <chantifa@windowslive.com>
2021-10-22 16:04:25 +0200

    update

--- app/frontend/src/pages/Home.js
+++ app/frontend/src/pages/Home.js
@@ -1,24 +1,23 @@
-import { React, useState, useEffect } from "react";
+import { React } from "react";
 import {
     Link
 } from "react-router-dom";
+import '../styles/HomeStyle.css';
+import fight from '../images/fight.jpeg';
 
 function Home() {
 
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        fetch("/api")
-            .then((res) => res.json())
-            .then((data) => setData(data.message));
-    }, []);
+    const welcomeText = "Coronattack is a funny two player game, in which each player tries either to infect people with a virus, or save them by vaccinating them! Go to the 'About' page to learn more. Press the 'get Started' to begin your journey! ";
 
     return (
-        <div>
-            <h1>Coronattack Home</h1>
-                <Link to="/login">Login</Link>
-            <br />
-            {data}
-        </div>
+            <div className="manageHomeDivs">
+                <h1 className="title">Coronattack</h1>
+                <div className="middleHomeDiv">
+                    <div className="welcomeTextStyle">{welcomeText}</div>
+                    <img src={fight} alt="virus-fight" className="homeImage" />
+                </div>
+                <Link to="/login" className="loginLink">Get Started</Link>
+            </div>
     )
 }
 
---
+++ app/frontend/src/pages/MainGame.js
@@ -0,0 +1,31 @@
+import {React, useState} from 'react';
+import '../styles/MainGameStyle.css';
+
+function MainGame(props) {
+
+    const [scoreA, setScoreA] = useState(0);
+    const [scoreB, setScoreB] = useState(0);
+
+    return (
+        <div>
+            <div style={{color: "white"}}>Game: {props.gameName}</div>
+            <div style={{color: "white"}}>PlayerA: {props.playerA}</div>
+            <div className="game"> 
+                <div className="playground">
+                    <div className="paddle-hand-right"></div>
+                    <div className="paddle-hand-left"></div>
+                    <div className="paddleA"></div>
+                    <div className="paddleB"></div>
+                    <div className="ball"></div>
+                </div>
+            </div>
+            <div className="scoreboard">
+                <div className="score"> A : {scoreA}</div>
+                <div className="score"> B : {scoreB}</div>
+            </div>
+
+        </div>
+    )
+}
+
+export default MainGame
