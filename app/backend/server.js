2046fa0765d91466c1826d853403c7b87d4e5495
greatmerlin <gmbaxevanos@gmx.ch>
2021-12-03 09:00:29 +0100

    meta tranfer from github repo

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,116 +1,116 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const router = express.Router();
 const app = express();
+
+// add router in express app
+app.use("/",router);
+
+//
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+//
+
 const { pool } = require("./dbConfig.js");
 const bcrypt = require("bcrypt");
+//
 
-// Middleware
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// create application/json parser
+const jsonParser = bodyParser.json()
 
-//const PORT = process.env.PORT || 3001;
+const cors = require('cors');
+app.use(cors());
 
-// test backend :3001 working
-app.get('/', (req, res) => {
-    res.send("Hello from backend!");
-});
-
-app.post('/users/register', async (req, res) => {
+router.post('/users/register', jsonParser, async (req, res) => {
 
     let { nickname, email, password, password2 } = req.body;
 
     //TODO: validation password - password2
 
-    //encrypt pass
-    hashedPassword = await bcrypt.hash(password, 10); // 10 is the number of rounds for the hash
+  //encrypt pass
+  hashedPassword = await bcrypt.hash(password, 10); // 10 is the number of rounds for the hash
 
-    // check if the email already exists in our db
-    pool.query(
-        `SELECT * FROM users
-            WHERE email = $1`,
-        [email],
-        (err, results) => {
-            if (err) {
-                console.log(err);
-            }
-            // if results.rows.length > 0  -----> account already exists
-            if (results.rows.length > 0) {
-                res.send({ emailExists: true })
-            } else {
-                // if the e-mail does not already exist in the db, we can add a new user to the db
-                pool.query(
-                    `INSERT INTO users (name, email, password)
-                    VALUES ($1, $2, $3)
-                    RETURNING id, name, email,  password`,
-                    [nickname, email, hashedPassword], // these are the values $1 $2 $3, we give them names
-                    (err, results) => {
-                        if (err) {
-                            throw err;
-                        }
-                    }
-                );
-            }
-        }
-    );
+  // check if the email already exists in our db
+  pool.query(
+      `SELECT * FROM users
+          WHERE email = $1`,
+      [email],
+      (err, results) => {
+          if (err) {
+              console.log(err);
+          }
+          // if results.rows.length > 0  -----> account already exists
+          if (results.rows.length > 0) {
+              res.send({ emailExists: true })
+          } else {
+              // if the e-mail does not already exist in the db, we can add a new user to the db
+              pool.query(
+                  `INSERT INTO users (name, email, password)
+                  VALUES ($1, $2, $3)
+                  RETURNING id, name, email,  password`,
+                  [nickname, email, hashedPassword], // these are the values $1 $2 $3, we give them names
+                  (err, results) => {
+                      if (err) {
+                          throw err;
+                      }
+                  }
+              );
+          }
+      }
+  );
 
 
 });
 
 // handle POST from LOGIN page
-app.post("/users/login", async (req, res) => {
+app.post("/users/login", jsonParser,  async (req, res) => {
 
-    let { name, email, password } = req.body;
+  let { email, password } = req.body;
 
-    pool.query(
-        `SELECT * FROM users WHERE email = $1`,
-        [email],
-        (err, results) => {
-            // if the e-mail exists in the DB, the user exist -> go further
-            if (results.rows.length > 0) {
-                const user = results.rows[0];
+  pool.query(
+    `SELECT * FROM users WHERE email = $1`,
+    [email],
+    (err, results) => {
+        // if the e-mail exists in the DB, the user exist -> go further
+        if (results.rows.length > 0) {
+            const user = results.rows[0];
 
-                bcrypt.compare(password, user.password, (err, isMatch) => {
-                    if (isMatch) {
-                        // password matches with the decrypted password in db
-                        res.send({ passwordIsCorrect: true, username: user.name })
-                    }
-                    else {
-                        // password does not match with the one in db
-                        // TODO: handle validation
-                        res.send({ passwordIsCorrect: false })
-                    }
-                });
-            }
-            else {
-                //if the email does not exist
-                console.log("EMAIL DOES NOT EXIST in DB");
-                // TODO: validation
-            }
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if (isMatch) {
+                    // password matches with the decrypted password in db
+                    res.send({ passwordIsCorrect: true, username: user.name })
+                }
+                else {
+                    // password does not match with the one in db
+                    // TODO: handle validation
+                    res.send({ passwordIsCorrect: false })
+                }
+            });
         }
-    );
+        else {
+            //if the email does not exist
+            console.log("EMAIL DOES NOT EXIST in DB");
+            // TODO: validation
+        }
+    }
+);
+
+  
 });
 
-//chat #######################################################
+// chat messages - Websockets communication
+io.on('connection', socket => {
+    socket.on('message', ({ name, message }) => {
+      io.emit('message', { name, message })
+    })
+  })
 
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+app.listen(3001,() => {
+console.log("Http Server started on PORT 3001");
+})
 
-// socket object -> to send messages to client
-io.on('connection', (socket) => {
-
-    // on to receive from the room with string, emit to send to this room
-    socket.on('chat from frontend', (msg) => {
-        io.emit('chat from backend', msg);
-        //console.log("takes the message from the frontend input field and saves it to 'msg' var via 'chat message' --> ", msg);
-    });
-
-});
-
-const WEBSOCKET_PORT = 3001;
-
-// CHAT LISTEN PORT
-server.listen(WEBSOCKET_PORT, () => {
-    console.log(`Backend Server runs on port ${WEBSOCKET_PORT}`);
-});
+server.listen(4000, () => {
+    console.log('Websockets listening on Port 4000');
+  });
--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
-import RoutingLogic from "./routing/RoutingLogic";
+import React, { useState } from 'react';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainBody from "./components/MainBody";
+import ChatRoom from "./components/ChatRoom";
+import About from "./components/About";
+import HowToPlay from "./components/HowToPlay";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import CreateGame from "./components/CreateGame";
+import Game from "./components/Game";
 
 function App() {
-  return (
-    <div>
-      <RoutingLogic />
-    </div>
-  );
-}
 
-export default App;
-
+	const [difficulty, setDifficulty] = useState(75);
+  
+	return (
+	  <Router>
+		<Routes>
+		  <Route path="/" element={< MainBody />} exact />
+		  <Route path="/chat" element={< ChatRoom />} />
+		  <Route path="/about" element={< About />} />
+		  <Route path="/howtoplay" element={< HowToPlay />} />
+		  <Route path="/login" element={ <Login /> } />
+		  <Route path="/register" element={ <Register /> } />
+		  <Route path="/creategame" element={ <CreateGame difficulty={difficulty} setDifficulty={setDifficulty} /> } />
+		  <Route path="/game" element={ < Game difficulty={difficulty}  /> } />
+		</Routes>
+	  </Router>
+	);
+  }
+  
+  export default App;
